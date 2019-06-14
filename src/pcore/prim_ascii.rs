bd355303032b305c4d5ebf900401eba5457359fe
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2019-06-14 11:34:01 -0700

    Move the enough-data check to the parsebuffer. This moves the check to one place instead of having it in every primitive.

--- src/pcore/prim_ascii.rs
+++ src/pcore/prim_ascii.rs
@@ -17,7 +17,6 @@ impl pbuf::ParsleyPrim for AsciiChar {
     fn prim_size_bytes() -> usize { 1 }
 
     fn parse_one(buf: &[u8]) -> Result<(Self::T, usize), pbuf::ParseError> {
-        if buf.len() == 0 { return Err(pbuf::ParseError::new("ascii: end-of-buffer")) };
         let c = char::try_from(buf[0]);
         if c.is_err() { return Err(pbuf::ParseError::new("ascii: invalid character")) };
         let c = c.unwrap();
