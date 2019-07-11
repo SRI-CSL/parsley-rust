ce4228b98fdeaa55d31532fd89497829fb568e66
Prashant Anantharaman <prashant.barca@gmail.com>
2019-07-11 09:16:00 -0400

    Add command line argument

--- src/parsley.rs
+++ src/parsley.rs
@@ -1,8 +1,21 @@
 extern crate parsley_rust;
 use parsley_rust::pcore::parsebuffer;
 use parsley_rust::pcore::prim_ascii;
+use std::env;
+use std::process;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut filename = String::new();
+    if args.len() == 2
+    {
+        filename = String::from(&args[1]);
+        println!("{}", filename);
+    }
+    else
+    {
+        process::exit(0x0100);
+    }
     let mut ascii_parser = prim_ascii::AsciiChar::new_guarded(Box::new(|c: &char| *c == 'A'));
     let mut v : Vec<u8> = Vec::new();
     v.push(65);   // 'A'
@@ -10,6 +23,6 @@ fn main() {
     v.push(0);    // nul; ascii
     let mut pb = parsebuffer::ParseBuffer::new(v);
 
-    println!("I'm using the library: {:?}", pb);
+    println!("I'm using the library: {:?} {}", pb, filename);
 }
 
