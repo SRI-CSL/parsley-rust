985217e8282e1759362859f41ba2989aafd71fa7
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2019-10-04 14:56:16 -0700

    Handle leading garbage in pdf printer, and change unit-test panics into application prints.

--- src/bin/pdf_printer.rs
+++ src/bin/pdf_printer.rs
@@ -14,8 +14,7 @@ fn parse_file(test_file: &str) {
     // Print current path
     let path = env::current_dir();
     if let Err(_) = path {
-        println!("Cannot get current dir!");
-        assert!(false);
+        panic!("Cannot get current dir!");
     }
     let mut path = path.unwrap();
     path.push(test_file);
@@ -25,25 +24,43 @@ fn parse_file(test_file: &str) {
     let mut file = match File::open(&path.as_path()) {
         // The `description` method of `io::Error` returns a string that
         // describes the error
-        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+        Err(why) => {
+            println!("couldn't open {}: {}", display, why.description());
+            process::exit(1)
+        },
         Ok(file) => file,
     };
 
     // Read the file contents into a string, returns `io::Result<usize>`
     let mut s = String::new();
     match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display, why.description()),
+        Err(why) => {
+            println!("couldn't read {}: {}", display, why.description());
+            process::exit(1)
+        },
         Ok(_)    => ()
     }
 
     let mut pb = ParseBuffer::new(Vec::from(s.as_bytes()));
-    assert_eq!(pb.get_cursor(), 0);
-    let buflen = pb.remaining();
 
+    // Handle leading garbage.
+    match pb.scan("%PDF-".as_bytes()) {
+        Ok(nbytes) => {
+            println!("Found {} bytes of leading garbage, dropping ...", nbytes);
+            pb.drop_upto()
+        },
+        Err(e) => {
+            println!("Cannot find header: {}", e);
+            process::exit(1)
+        }
+    }
+
+    let buflen = pb.remaining();
     let mut p = HeaderP;
     let hdr = p.parse(&mut pb);
     if let Err(_) = hdr {
-        panic!("Unable to parse header from {}: {:?}", display, hdr)
+        println!("Unable to parse header from {}: {:?}", display, hdr);
+        process::exit(1)
     }
     // Todo: some sanity check of header.
 
@@ -51,7 +68,8 @@ fn parse_file(test_file: &str) {
     pb.set_cursor(buflen);
     let eof = pb.backward_scan("%%EOF".as_bytes());
     if let Err(_) = eof {
-        panic!("Could not find %%EOF in {}: {:?}", display, eof);
+        println!("Could not find %%EOF in {}: {:?}", display, eof);
+        process::exit(1)
     }
     let eof_ofs = buflen - eof.unwrap();
     println!("Found %%EOF at offset {}.", eof_ofs);
@@ -59,15 +77,17 @@ fn parse_file(test_file: &str) {
     // Scan backward for startxref.
     let sxref = pb.backward_scan("startxref".as_bytes());
     if let Err(_) = sxref {
-        panic!("Could not find startxref in {}: {:?}", display, sxref);
+        println!("Could not find startxref in {}: {:?}", display, sxref);
+        process::exit(1)
     }
     let sxref_ofs = buflen - sxref.unwrap();
     println!("Found startxref at offset {}.", sxref_ofs);
     let mut p = StartXrefP;
     let sxref = p.parse(&mut pb);
     if let Err(_) = sxref {
-        panic!("Could not parse startxref in {} at pos {}: {:?}",
-               display, pb.get_cursor(), sxref);
+        println!("Could not parse startxref in {} at pos {}: {:?}",
+                 display, pb.get_cursor(), sxref);
+        process::exit(1)
     }
     let sxref = sxref.unwrap();
     println!(" startxref span: {}..{}.", sxref.parsebuf_start(), sxref.parsebuf_end());
@@ -79,8 +99,9 @@ fn parse_file(test_file: &str) {
     let mut p = XrefSectP;
     let xref = p.parse(&mut pb);
     if let Err(_) = xref {
-        panic!("Could not parse xref in {} at pos {}: {:?}",
-               display, pb.get_cursor(), xref);
+        println!("Could not parse xref in {} at pos {}: {:?}",
+                 display, pb.get_cursor(), xref);
+        process::exit(1)
     }
     let xref = xref.unwrap().unwrap();
     let mut offsets = Vec::new();
@@ -107,8 +128,9 @@ fn parse_file(test_file: &str) {
         pb.set_cursor((*o).try_into().unwrap());
         let lobj = p.parse(&mut pb);
         if let Err(_) = lobj {
-            panic!("Cannot parse object at offset {} in {}: {:?}",
-                   o, display, lobj)
+            println!("Cannot parse object at offset {} in {}: {:?}",
+                     o, display, lobj);
+            process::exit(1)
         }
         let lobj = lobj.unwrap();
         let obj = lobj.unwrap();
