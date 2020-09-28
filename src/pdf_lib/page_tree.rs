0448ebb442c0cf12449beec15605574714dc7174
Prashant Anantharaman <prashant.barca@gmail.com>
2020-09-28 17:50:09 -0400

    Non root page tree node added

--- src/pdf_lib/page_tree.rs
+++ src/pdf_lib/page_tree.rs
@@ -47,6 +47,79 @@ mod test_page_tree {
             )))
     }
 
+    // Page Tree Non-Root Node Tests
+    #[test]
+    fn test_non_root_page_tree() {
+        let mut ctxt = PDFObjContext::new();
+        let mut p = PDFObjP::new(&mut ctxt);
+        //let v = Vec::from("<</Type /Pages /Kids [4 0 R  10 0 R 24 0 R ] /Count 3 >>".as_bytes());
+        let v = Vec::from("<</Type /Pages /Kids [4 0 R  10 0 R 24 0 R ] /Count 3 >>".as_bytes());
+        //let v = Vec::from("<< /Count 3 >>".as_bytes());
+        let mut pb = ParseBuffer::new(v);
+        let obj = p.parse(&mut pb).unwrap();
+        let pages = DictEntry {
+            key: Vec::from("Type"),
+            chk: mk_pages_check(), // this must be a NameT
+            opt: DictKeySpec::Required,
+        };
+        let count = DictEntry {
+            key: Vec::from("Count"),
+            chk: mk_count_typchk(),
+            opt: DictKeySpec::Required,
+        };
+        let kids = DictEntry {
+            key: Vec::from("Kids"),
+            chk: mk_indirect_typchk(), 
+            opt: DictKeySpec::Required,
+        };
+        let parent = DictEntry {
+            key: Vec::from("Parent"),
+            chk: mk_indirect_typchk(),
+            opt: DictKeySpec::Required,
+        };
+        let typ = TypeCheck::new(Rc::new(PDFType::Dict(vec![pages, count, kids, parent])));
+        assert_eq!(
+            check_type(&ctxt, Rc::new(obj), Rc::new(typ)),
+            Some(TypeCheckError::MissingKey([80, 97, 114, 101, 110, 116].to_vec()))
+        );
+    }
+    #[test]
+    fn test_non_root_page_tree_not_wrong() {
+        let mut ctxt = PDFObjContext::new();
+        let mut p = PDFObjP::new(&mut ctxt);
+        //let v = Vec::from("<</Type /Pages /Kids [4 0 R  10 0 R 24 0 R ] /Count 3 >>".as_bytes());
+        let v = Vec::from("<</Type /Pages /Parent [4 0 R] /Kids [4 0 R  10 0 R 24 0 R ] /Count 3 >>".as_bytes());
+        //let v = Vec::from("<< /Count 3 >>".as_bytes());
+        let mut pb = ParseBuffer::new(v);
+        let obj = p.parse(&mut pb).unwrap();
+        let pages = DictEntry {
+            key: Vec::from("Type"),
+            chk: mk_pages_check(), // this must be a NameT
+            opt: DictKeySpec::Required,
+        };
+        let count = DictEntry {
+            key: Vec::from("Count"),
+            chk: mk_count_typchk(),
+            opt: DictKeySpec::Required,
+        };
+        let kids = DictEntry {
+            key: Vec::from("Kids"),
+            chk: mk_indirect_typchk(), 
+            opt: DictKeySpec::Required,
+        };
+        let parent = DictEntry {
+            key: Vec::from("Parent"),
+            chk: mk_indirect_typchk(),
+            opt: DictKeySpec::Required,
+        };
+        let typ = TypeCheck::new(Rc::new(PDFType::Dict(vec![pages, count, kids, parent])));
+        assert_eq!(
+            check_type(&ctxt, Rc::new(obj), Rc::new(typ)),
+            None
+        );
+    }
+    // Page Tree Non Root Node Tests End
+
     // Page Tree Root Node Tests
     #[test]
     fn test_page_tree() {
