954e833db9b49f22b33248354d8c0c866dc2ac20
sameedali <sameed.ali.94@gmail.com>
2020-11-13 02:28:44 -0500

    code upload for reference
    Signed-off-by: sameedali <sameed.ali.94@gmail.com>

--- src/pdf_lib/pdf_streams.rs
+++ src/pdf_lib/pdf_streams.rs
@@ -921,4 +921,29 @@ endobj");
             assert!(false);
         }
     }
+
+    #[test]
+    fn test_ascii85decode() {
+        let v = get_test_data("tests/test_files/ascii85decode.obj");
+        let mut pb = ParseBuffer::new(v);
+        let mut ctxt = mk_new_context();
+        let mut p = IndirectP::new(&mut ctxt);
+        let io = p.parse(&mut pb).expect("unable to parse stream");
+        let io = io.val();
+        if let PDFObjT::Stream(ref s) = io.obj().val() {
+            let content = s.stream().val();
+            let mut stream_buf = ParseBuffer::new_view(&pb, content.start(), content.size());
+            let mut sp = XObjectStreamP::new(&mut ctxt, s);
+            let st = sp.parse(&mut stream_buf);
+            let st = st.unwrap();
+            // let mut xrsp = XrefStreamP::new(s);
+            // let xrt = xrsp.parse(&mut xref_buf).unwrap();
+            // let ents = xrt.val().ents();
+            // let size = s.dict().val().get_usize(b"Size").unwrap();
+            // assert_eq!(size, ents.len())
+            assert_eq!(ost.val().objs().len(), 73)
+        } else {
+            assert!(false);
+        }
+    }
 }
