231f92032cedb75bd82cbc50649fb3552e3479ed
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2021-01-27 14:20:36 -0800

    Add missed copyright headers.

--- src/pdf_lib/catalog.rs
+++ src/pdf_lib/catalog.rs
@@ -1,3 +1,21 @@
+// Copyright (c) 2019-2021 SRI International.
+// All rights reserved.
+//
+//    This file is part of the Parsley parser.
+//
+//    Parsley is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Parsley is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use super::pdf_obj::PDFObjT;
 use crate::pdf_lib::common_data_structures::{
     mk_array_of_dict_typchk, mk_generic_array_typchk, mk_generic_dict_typchk,
